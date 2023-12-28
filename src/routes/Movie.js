64f37e7eddc78a24f5a8f2ca4ab7c2b365886c87
yougmi <zoz7353@gmail.com>
2023-12-28 11:48:59 +0900

    :seedling:  Local Only Fields

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -9,6 +9,7 @@ const GET_MOVIE = gql`
       title
       medium_cover_image
       rating
+      isLiked @client
     }
   }
 `;
@@ -26,6 +27,7 @@ const Movie = () => {
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button>{data?.movies?.isLiked ? "Unlike" : "Like"}</button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
