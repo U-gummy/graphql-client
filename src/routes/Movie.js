a86acef364c89db806199be2fed2dc3119ae8ed8
yougmi <zoz7353@gmail.com>
2023-12-28 11:32:44 +0900

    :heavy_plus_sign: useQuery Variables

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,5 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 const Movie = () => {
-  return <div>Movie</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, { variables: { movieId: id } });
+  console.log("data: ", data);
+
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return (
+    <div>
+      {data.movie.title}
+      {data.movie.id}
+    </div>
+  );
 };
 
 export default Movie;
